dc6ebc982a3a71f506f9520ef10a047f385ac288
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-02-15 09:56:57 +0530

    add virtual field for movie duration in hours

--- Models/movieModels.js
+++ Models/movieModels.js
@@ -63,8 +63,15 @@ const movieSchema=new mongoose.Schema({
         required:[true,'Price is required'],
     }
 
+},
+{
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
 });
 
+movieSchema.virtual('durationInHours').get(function(){
+    return this.duration/60;
+})
 
 const Movie=mongoose.model('Movie',movieSchema);
 module.exports=Movie;
