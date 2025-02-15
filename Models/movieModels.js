3dbccab9b97ef1cbd6d9d7a4c62daa92949fb03f
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-02-15 10:32:58 +0530

    add createdBy field and logging for new movie entries in log.txt file

--- Models/movieModels.js
+++ Models/movieModels.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
-
+const fs=require('fs');
+const e = require('express');
 
 // const mongoose=require('mongoose');
 
@@ -45,6 +46,9 @@ const movieSchema=new mongoose.Schema({
         default:Date.now,
         select:false,
     },
+    createdBy:{
+        type:String,
+    },
     directors:{
         type:[String],
         required:[true,'Director is required'],
@@ -71,7 +75,21 @@ const movieSchema=new mongoose.Schema({
 
 movieSchema.virtual('durationInHours').get(function(){
     return this.duration/60;
-})
+});
+movieSchema.pre('save', function(next) {
+    this.createdBy = "robert";
+    next();
+});
+movieSchema.post('save', function(doc, next) {
+    const content=`a new movie has been added with the name ${doc.name} created by ${doc.createdBy}\n`;
+    fs.writeFileSync('./log/log.txt',content,{flag:'a'},(err)=>{
+        if(err){
+            console.log(err.message);
+        }
+    });
+
+    next();
+});
 
 const Movie=mongoose.model('Movie',movieSchema);
 module.exports=Movie;
