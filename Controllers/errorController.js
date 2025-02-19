14573077bf48d472e9cc1a0d10cf95c6c7ed1230
chris-robert-yeslin2006 <robertchemsit2006@gmail.com>
2025-02-19 18:14:05 +0530

    Development & Production Error Handling

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,8 +1,34 @@
-module.exports = (error, req, res, next) => {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.statusCode,
-        message: error.message
+const devError = (err, res) => {    
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message,
+        stackTrace:err,
+        error: err,
     });
+}
+const prodError = (err, res) => {
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.statusCode,
+            message: err.message,
+        });
+       
+    }else{
+        res.status(500).json({
+            status: 'error',
+            message: "Something went wrong",
+            
+        });
+    }
+    
+}
+
+module.exports = (error, req, res, next) => {
+    if(process.env.NODE_ENV==='development'){
+        devError(error, res);
+    }else if(process.env.NODE_ENV==='production'){
+        prodError(error, res);
+
+    }
+    
 };
