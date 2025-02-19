ba3f708ab5a3ec06773809090c3b40d9a519952e
chris-robert-yeslin2006 <robertchemsit2006@gmail.com>
2025-02-19 15:44:53 +0530

    added a global error handler

--- app.js
+++ app.js
@@ -12,9 +12,23 @@ app.use(express.static('./public'))
 //USING ROUTES
 app.use('/api/v1/movies', moviesRouter);
 app.get('*',(req,res) => {
-    res.status(404).json({
-        status: 'fail',
-        message: 'Invalid URL'
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: 'Invalid URL'
+    // });
+    const err=new Error('Invalid URL');
+    err.statusCode=404;
+    err.status='fail';
+    next(err);
+
+});
+
+app.use((error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message
     });
 });
 
