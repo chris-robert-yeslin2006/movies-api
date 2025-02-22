a2847f0bf55d3a5d8fc81d158b31affbb50d1819
chris-robert-yeslin2006 <robertchemsit2006@gmail.com>
2025-02-22 16:36:41 +0530

    Signing up a User

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,11 +1,17 @@
 const User = require('../models/userModel');
 const asyncErrorHandler = require('../utils/asyncErrorHandler');
+const jwt = require('jsonwebtoken');
 
 exports.signup = asyncErrorHandler(async (req, res, next) => {
         const newUser = await User.create(req.body);
 
+        const token = jwt.sign({ id: newUser._id }, process.env.SECRET_STR, {
+            expiresIn: process.env.LOGIN_EXPIRES,
+        });
+
         res.status(200).json({
             status:"success",
+            token,
             message:"User created successfully",
             data:newUser
         })
